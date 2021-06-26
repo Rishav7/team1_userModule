77be76395e7bade0dd049bb433467a233d203f27
Rishav7 <rajrishav381.rr@gmail.com>
2021-06-26 19:32:16 +0530

    forgot password update

---
+++ frontend-app/src/App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { BrowserRouter as Router , Route} from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footers from './components/Footers';
+
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
+import Homepage from './components/Homepage';
+import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
+import NewPasswordScreen from './screens/NewPasswordScreen';
+
+
+
+
+const App = () => {
+  return (
+    
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+         <Route path='/login' component={LoginScreen}></Route>
+         <Route path='/register' component={RegisterScreen}></Route>
+         <Route path='/profile' component={ProfileScreen} />
+         <Route path='/forgotPassword' component={ForgotPasswordScreen} exact />
+         <Route path='/resetPassword/:token' component={NewPasswordScreen} exact />
+
+         </Container>
+      </main>
+      <Footers />
+
+
+    </Router>
+  );
+};
+
+export default App;
