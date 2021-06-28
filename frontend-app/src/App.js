b00fb73acd6f905e293c23907f4f10778e32d9b9
Rishav7 <rajrishav381.rr@gmail.com>
2021-06-28 10:53:27 +0530

    check
    Signed-off-by: Rishav7 <rajrishav381.rr@gmail.com>

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router , Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 import Header from './components/Header';
 import Footers from './components/Footers';
@@ -7,7 +7,7 @@ import Footers from './components/Footers';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
-import Homepage from './components/Homepage';
+
 import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
 import NewPasswordScreen from './screens/NewPasswordScreen';
 
@@ -16,18 +16,18 @@ import NewPasswordScreen from './screens/NewPasswordScreen';
 
 const App = () => {
   return (
-    
+
     <Router>
       <Header />
       <main className="py-3">
         <Container>
-         <Route path='/login' component={LoginScreen}></Route>
-         <Route path='/register' component={RegisterScreen}></Route>
-         <Route path='/profile' component={ProfileScreen} />
-         <Route path='/forgotPassword' component={ForgotPasswordScreen} exact />
-         <Route path='/resetPassword/:token' component={NewPasswordScreen} exact />
+          <Route path='/login' component={LoginScreen}></Route>
+          <Route path='/register' component={RegisterScreen}></Route>
+          <Route path='/profile' component={ProfileScreen} />
+          <Route path='/forgotPassword' component={ForgotPasswordScreen} exact />
+          <Route path='/resetPassword/:token' component={NewPasswordScreen} exact />
 
-         </Container>
+        </Container>
       </main>
       <Footers />
 
