df1ba5a6c988325a2a849fa8e3f02000bebb7db0
Vaishnavi-Mandhana <mandhanavs@rknec.edu>
2021-07-01 14:23:35 +0530

    no change

--- frontend-app/src/App.js
+++ frontend-app/src/App.js
@@ -11,7 +11,6 @@ import ProfileScreen from './screens/ProfileScreen';
 import ForgotPasswordScreen from './screens/ForgotPasswordScreen';
 import NewPasswordScreen from './screens/NewPasswordScreen';
 
-//change
 const App = () => {
   return (
 
